001ee736ec95a7cadd9364700891dea6abe10f71
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-26 13:16:35 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+//Initializing the array to store books
+let myLibrary = [];
+
+function Book() {
+  // the book constructor...
+}
+
+function addBookToLibrary() {
+  // Adding books to the library
+}
