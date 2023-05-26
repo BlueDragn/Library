121b9a9fe9ac5fb0af1786791cd7812310acfe5f
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-26 18:00:19 +0530

    Initial commit to update  the  UI

--- src/script.js
+++ src/script.js
@@ -1,7 +1,5 @@
 //Initializing the array to store books
 let myLibrary = [];
-
-
 function Book(title, author, pages, hasRead) {
     // the book constructor...
   this.title = title;
@@ -16,16 +14,46 @@ Book.prototype.info = function() {
 };
 
 function addBookToLibrary() {
-    // Adding books to the library
-  var title = ''
-  var author = '';
-  var pages = '';
-  var hasRead = '';
+  var titleInput = document.querySelector('input[name="title"]').value;
+  var authorInput = document.querySelector('input[name="author"]').value;
+  var pagesInput = parseInt(document.querySelector('input[name="pages"]').value);
+  var hasReadInput = document.querySelector('input[name="read-status"]:checked');
 
-  var newBook = new Book(title, author, pages, hasRead);
+  var hasRead = hasReadInput ? true : false;
+
+  var newBook = new Book(titleInput, authorInput, pagesInput, hasRead);
   myLibrary.push(newBook);
+
+  // Clear input fields after adding book
+  document.querySelector('input[name="title"]').value = '';
+  document.querySelector('input[name="author"]').value = '';
+  document.querySelector('input[name="pages"]').value = '';
+  document.querySelector('input[name="read-status"]').checked = false;
+
+  displayBooks();
 }
 
-// Example usage:
-addBookToLibrary();
-console.log(myLibrary); // Output: Array containing the newly added book object
+function displayBooks() {
+  var cardsContainer = document.querySelector('.cards-container');
+  cardsContainer.innerHTML = ''; // Clear existing cards
+
+  myLibrary.forEach(function(book) {
+    var card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <h2>Title: ${book.title}</h2>
+      <h2>Author: ${book.author}</h2>
+      <h2>Pages: ${book.pages}</h2>
+      <button class="read-btn">${book.hasRead ? 'Read' : 'Not Read'}</button>
+    `;
+
+    cardsContainer.appendChild(card);
+  });
+}
+
+// Add event listener to the Add button
+var addBtn = document.querySelector('.add-btn');
+addBtn.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent form submission
+  addBookToLibrary();
+});
