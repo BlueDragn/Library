453040359d8c26f2f46e4ecc84086ee00418251e
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-26 15:06:51 +0530

    added book to Library using prompt

--- src/script.js
+++ src/script.js
@@ -1,10 +1,31 @@
 //Initializing the array to store books
 let myLibrary = [];
 
-function Book() {
-  // the book constructor...
+
+function Book(title, author, pages, hasRead) {
+    // the book constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.hasRead = hasRead;
 }
 
+Book.prototype.info = function() {
+  var readStatus = this.hasRead ? "read" : "not read yet";
+  return this.title + " by " + this.author + ", " + this.pages + " pages, " + readStatus;
+};
+
 function addBookToLibrary() {
-  // Adding books to the library
+    // Adding books to the library
+  var title = prompt("Enter the title of the book:");
+  var author = prompt("Enter the author of the book:");
+  var pages = parseInt(prompt("Enter the number of pages in the book:"));
+  var hasRead = confirm("Have you read the book?");
+
+  var newBook = new Book(title, author, pages, hasRead);
+  myLibrary.push(newBook);
 }
+
+// Example usage:
+addBookToLibrary();
+console.log(myLibrary); // Output: Array containing the newly added book object
